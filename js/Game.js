be8c7517f745fccfb09fe6737c8a608f24ecbe9c
Alex Szczerba <Szczerba18@icloud.com>
2022-01-11 17:40:55 -0700

    added phrases

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor(missed, phrases, activePhrase) {
+        this.missed = 0;
+        this.activePhrase = 'null';
+        this.phrases = [
+            'Every cloud has a silver lining', 
+            'Your guess is as good as mine', 
+            'Ignorance is bliss',
+            'Slow and steady wins the race',
+            'He who laughs last laughs loudest'
+        ];
+    }
+
+/**
+* Creates phrases for use in game
+* @return {array} An array of phrases that could be used in the game
+*/
+    // createPhrases() {
+
+    // };
+
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const game = new Game();
+game.phrases.forEach((phrase, index) => {
+console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});
